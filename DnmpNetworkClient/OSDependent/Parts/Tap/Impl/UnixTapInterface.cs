3e3e22d06a00753f480ba93edf3248db23e2c78a
Egor Koleda <radioegor146@yandex.ru>
2019-03-25 06:56:17 +0300

    gcc union fix

--- DnmpNetworkClient/OSDependent/Parts/Tap/Impl/UnixTapInterface.cs
+++ DnmpNetworkClient/OSDependent/Parts/Tap/Impl/UnixTapInterface.cs
@@ -9,49 +9,13 @@ namespace DnmpNetworkClient.OSDependent.Parts.Tap.Impl
 {
     internal class UnixTapInterface : ITapInterface
     {
-        [StructLayout(LayoutKind.Sequential)]
+        [StructLayout(LayoutKind.Sequential, Size = 40)]
         private struct IfReq
         {
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
             public readonly string Name;
 
-            private readonly SockAddr Addr;
-            private readonly SockAddr DstAddr;
-            private readonly SockAddr BroadAddr;
-            private readonly SockAddr NetMask;
-            public readonly SockAddr HwAddr;
-
             public short Flags;
-            private readonly int IValue;
-            private readonly int Mtu;
-
-            private readonly IfMap Map;
-
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
-            private readonly string Slave;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
-            private readonly string NewName;
-            private readonly string Data;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct IfMap
-        {
-            private readonly ulong MemStart;
-            private readonly ulong MemEnd; 
-            private readonly ushort BaseAddr;
-            private readonly byte Irq;
-            private readonly byte Dma;
-            private readonly byte Port;
-        };
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct SockAddr
-        {
-            private readonly ushort Family;
-
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 14)]
-            public readonly byte[] Data;
         }
 
         private const int tapIff = 0x0002;
@@ -67,7 +31,7 @@ namespace DnmpNetworkClient.OSDependent.Parts.Tap.Impl
 
         public PhysicalAddress GetPhysicalAddress()
         {
-            return new PhysicalAddress(currentInterfaceInfo.HwAddr.Data);
+            return new PhysicalAddress(new byte[6]);
         }
 
         public Stream Open()

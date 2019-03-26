b263d1ec3704275fcb3b122fcd9d12feff99baf2
Egor Koleda <radioegor146@yandex.ru>
2019-03-26 19:01:32 +0300

    Update UnixRuntime.cs

--- DnmpNetworkClient/OSDependent/Parts/Runtime/Impl/UnixRuntime.cs
+++ DnmpNetworkClient/OSDependent/Parts/Runtime/Impl/UnixRuntime.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using Mono.Unix.Native;
@@ -16,9 +17,21 @@ namespace DnmpNetworkClient.OSDependent.Parts.Runtime.Impl
         public void PreInit()
         {
             var descriptor = Syscall.open("/dev/net/tun", OpenFlags.O_RDWR);
+            
             if (descriptor >= 0)
-                return;
-            logger.Error("/dev/net/tun open error! Check availability of TAP/TUN!");
+            {
+                logger.Error("/dev/net/tun open error! Check availability of TAP/TUN!");
+                Environment.Exit(0);
+            }
+
+            using (var identity = WindowsIdentity.GetCurrent())
+            {
+                var principal = new WindowsPrincipal(identity);
+                if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+                    return;
+            }
+
+            logger.Error("Need sudo for TUN/TAP interface!");
             Environment.Exit(0);
         }
 
